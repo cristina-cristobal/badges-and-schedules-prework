d764519fa6bf5624fe5552ddd28e7e2ed7795b8d
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-27 22:44:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,3 +21,12 @@ def assign_rooms(attendees)
   end
     room_messages
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |id|
+    puts id
+  end
+  assign_rooms(attendees).each do |id|
+    puts id
+  end
+end
